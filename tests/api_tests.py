0cfa00a59b6dfc38d39d73dfbcd7178cae401b11
cavens <jef@cavens.io>
2015-03-05 03:49:52 +0000

    your base are belong to us

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -36,4 +36,109 @@ class TestAPI(unittest.TestCase):
         # Delete test upload folder
         shutil.rmtree(upload_path())
 
+    def testGetSongs(self):
+        """Gets list of songs"""
 
+        # Populate DB
+        file_1 = models.File(name="filename_1.mp3")
+        song_1 = models.Song()
+        file_1.song = song_1
+
+        file_2 = models.File(name="filename_2.mp3")
+        song_2 = models.Song()
+        file_2.song = song_2
+        
+        session.add_all([song_1,song_2,file_1,file_2])        
+        session.commit()
+        
+        # Send stuff to get endpoint
+        response = self.client.get("/api/songs",headers=[("Accept", "application/json")])
+        
+        # General assertions
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.mimetype, "application/json")  
+      
+        # Data assetions
+        data = json.loads(response.data)
+        self.assertEqual(len(data),2)
+        
+        song_1 = data[0]
+        self.assertEqual(song_1["file"]["name"],"filename_1.mp3")
+        song_2 = data[1]
+        self.assertEqual(song_2["file"]["name"],"filename_2.mp3")
+
+    def testPostSong(self):
+        """Posts a song"""
+        
+        # Populate JSON
+        data = {"name":"filename_1.mp3"}
+
+        # Send stuff to POST endpoint
+        response = self.client.post("/api/songs", data=json.dumps(data), content_type="application/json",headers=[("Accept", "application/json")])
+      
+        # General assertions
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+
+        # Check content response
+        data = json.loads(response.data)
+        self.assertEqual(data["id"], 1)
+        self.assertEqual(data["file"]["name"], "filename_1.mp3")
+
+        songs = session.query(models.Song).all()
+        self.assertEqual(len(songs), 1)
+
+        song = songs[0]
+        self.assertEqual(song.id, 1)
+        self.assertEqual(song.file.name, "filename_1.mp3")
+
+        
+    def testUnsupportedAcceptHeader(self):
+        response = self.client.get("/api/songs",
+            headers=[("Accept", "application/xml")]
+        )
+
+        self.assertEqual(response.status_code, 406)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data)
+        self.assertEqual(data["message"],
+                         "Request must accept application/json data")
+        
+
+    def test_get_uploaded_file(self):
+        path =  upload_path("test.txt")
+        with open(path, "w") as f:
+            f.write("File contents")
+
+        response = self.client.get("/uploads/test.txt",headers=[("Accept", "application/json")])
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "text/plain")
+        self.assertEqual(response.data, "File contents")
+
+    def test_file_upload(self):
+        data = {
+            "file": (StringIO("File contents"), "test.txt")
+        }
+
+        #response = self.client.post("/api/files",data=data,content_type="multipart/form-data")
+        print "EEN"
+        response = self.client.post("/api/files",data=data,content_type="multipart/form-data", headers=[("Accept", "application/json")])
+
+        print "ZES"        
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+
+        print "ZEVEN"                
+        data = json.loads(response.data)
+        self.assertEqual(urlparse(data["path"]).path, "/uploads/test.txt")
+
+        print "ACHT"        
+        path = upload_path("test.txt")
+        self.assertTrue(os.path.isfile(path))
+        with open(path) as f:
+            contents = f.read()
+        self.assertEqual(contents, "File contents")        
+        
+        
--- tuneful/api.py
+++ tuneful/api.py
@@ -12,3 +12,67 @@ from database import session
 from utils import upload_path
 
 
+@app.route("/api/songs", methods=["GET"])
+@decorators.accept("application/json")
+def songs_get():
+    
+    # Get the posts from the database
+    songs = session.query(models.Song).all()
+
+    # Convert the songs to JSON and return a response
+    data = json.dumps([song.as_dictionary() for song in songs])
+    return Response(data, 200, mimetype="application/json")
+  
+
+  
+@app.route("/api/songs", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def songs_post():
+    
+    # Retreive data
+    data = request.json
+
+    # Post song
+    file_1 = models.File(name=data["name"])
+    song_1 = models.Song()
+    file_1.song = song_1
+
+    session.add(file_1,song_1)
+    session.commit()
+    
+    # Construct and send back response
+    data = json.dumps(file_1.song.as_dictionary())
+    return Response(data,201,mimetype="application/json")
+
+
+@app.route("/uploads/<filename>", methods=["GET"])
+@decorators.accept("application/json")
+def uploaded_file(filename):
+    return send_from_directory(upload_path(), filename)  
+  
+  
+  
+@app.route("/api/files", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("multipart/form-data")
+def file_post():
+    file = request.files.get("file")
+    print file
+    if not file:
+        data = {"message": "Could not find file data"}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+    filename = secure_filename(file.filename)
+    db_file = models.File(name=filename)
+    session.add(db_file)
+    session.commit()
+    file.save(upload_path(filename))
+    print filename
+    print "VIJFENHALF"
+    data = db_file.as_dictionary()
+    print "BIJNAZES"
+    return Response(json.dumps(data), 201, mimetype="application/json")
+
+  
+  
+  
